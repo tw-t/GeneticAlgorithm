c2cab356911806f9b5c8ca31c298b17020fd3361
TW <49590849+tw-t@users.noreply.github.com>
2022-11-03 23:50:29 +1100

    Update g.py
    Added duplication counter

--- g.py
+++ g.py
@@ -6,18 +6,23 @@ NUM_OF_TRUCKS = 3
 NUM_OF_ITEMS = 35
 TRUCK_CAPACITY = 19
 
-POPULATION = 100;
-MUTATIONPERCENT=3 
+POPULATION = 100
+MUTATIONPERCENT=5 
 
-generation=0;  
-maxGeneration =20;
-marker = -99999;
-mutations = 0;
-best = -1; # best individual's ID
+generation=0 
+maxGeneration =20
+marker = -99999
+mutations = 0
+best = -1 # best individual's ID
 bestScore = -99991 # score of best individual
 
-globalRand=random.Random() # use globalRand.random
-random.seed(102) #comment in for fixed seed for repeatability
+dup_count=0
+
+# Best run no hosp:4011, 4111; Best run hosp: 385; full load:4010 (N +2)
+seed_no= 105 # Best run no hosp: 105 ; Best run hosp: 125 ; full load: 101 (N +1)
+
+globalRand=random.Random()
+globalRand.seed(seed_no) #comment in for fixed seed for repeatability
 
 population = None # will initialise later dont worry
 
@@ -38,8 +43,3 @@ def rnd0or1 (rng):
     retv = 0
     if (rng.random() <0.5): retv=1
     return retv
-
-
-
-
-
